8d5edd5956972c0c0706cbf935f34c4dc85055c2
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-01-31 03:45:43 +0600

    70%work done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        main: "url('./c1-assets/cat-big.png')",
+      },
+    },
+  },
+  plugins: [],
+};
